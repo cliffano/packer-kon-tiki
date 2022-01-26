fad831081ef88b4cb45256cfd635c71f42eac709
Cliffano Subagio <cliffano@gmail.com>
2022-01-26 22:09:19 +1100

    gs

--- .github/actions/publish-docker-kon-tiki-private-action/entrypoint.sh
+++ .github/actions/publish-docker-kon-tiki-private-action/entrypoint.sh
@@ -4,5 +4,5 @@ source /home/.virtualenvs/py36/bin/activate
 make clean deps lint build-docker-kon-tiki-private
 cat logs/kon-tiki-private.log
 echo "${GITHUB_TOKEN}" | docker login ghcr.io --username cliffano --password-stdin
-docker inspect ghcr.io/shinesolutions/kon-tiki
+docker inspect ghcr.io/cliffano/kon-tiki
 make publish-docker-kon-tiki-private
