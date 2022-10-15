1c1677f0a6c1b3e4bc1fe801f2d5f080e9ff24a7
Cliffano Subagio <cliffano@gmail.com>
2022-10-15 23:50:52 +1100

    Add info script to list command versions.

---
+++ provisioners/info.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -o errexit
+
+export PATH=/opt/puppetlabs/puppet/bin:/opt/puppetlabs/bin:/usr/local/go/bin:/root/.cargo/bin/:/usr/local/sbin:/usr/local/bin:$PATH
+
+echo "****************************************"
+echo "* Provisioned software info"
+echo "****************************************"
+echo "PATH: $PATH"
+echo "puppet version: $(puppet --version)"
+echo "facter version: $(facter --version)"
+echo "hiera version: $(hiera --version)"
+echo "go version: $(go version)"
+echo "terraform version: $(terraform version)"
+echo "packer version: $(packer version)"
+echo "checkmake version: $(checkmake --version)"
+echo "python version: $(python --version)"
+echo "ruby version: $(ruby --version)"
+echo "gem version: $(gem --version)"
+echo "bundler version: $(bundler --version)"
+echo "rspec version: $(rspec --version)"
+echo "imagemagick convert version: $(convert --version)"
+echo "imagemagick mogrify version: $(mogrify --version)"
